c015d14c77b33fe5b70941788817aac99502c236
Shane O'Moore <shaneiadt@gmail.com>
2022-12-04 12:39:56 +0000

    feat: scraper & currency conversion

---
+++ currency.js
@@ -0,0 +1,26 @@
+import fetch from "node-fetch";
+
+const apiKey = "QXFJBMeGxXItq0ubSTxIf1WMCslueoFQ";
+const date = new Date().toISOString().split("T")[0];
+
+async function getExchangeRate() {
+  const response = await fetch(
+    `https://api.apilayer.com/currency_data/change?start_date=${date}&end_date=${date}&currencies=USD,EUR`,
+    {
+      method: "GET",
+      redirect: "follow",
+      headers: {
+        apiKey,
+      },
+    }
+  );
+  const rates = await response.json();
+
+  return rates.quotes["USDEUR"].end_rate;
+}
+
+const rate = await getExchangeRate();
+const dollars = 100;
+const euros = (dollars * rate).toFixed(2);
+
+console.log(euros);
---
+++ index.js
@@ -0,0 +1,51 @@
+import requestPromise from "request-promise";
+import * as cheerio from "cheerio";
+import { writeFile } from "fs";
+
+async function scrape() {
+  console.log("Downloading...");
+  
+  const releaseDate = new Date().toISOString().split("T")[0];
+  const items = [];
+  const paginationMax = 500;
+
+  for (let index = 0; index <= paginationMax; index += 50) {
+    const url = `https://www.pricecharting.com/console/apple-iphone?sort=popularity&release-date=${releaseDate}&cursor=${index}`;
+    const html = await requestPromise.get(url);
+    const $ = cheerio.load(html);
+
+    $("tr[id^='product']").each((_, product) => {
+      const id = $(product).attr("id");
+      const title = $(`#${id} .title`).text().trim();
+      const url = $(`#${id} .title a`).attr("href");
+      const usedPrice = $(`#${id} .used_price`).text().trim();
+      const refurbPrice = $(`#${id} .cib_price`).text().trim();
+      const newPrice = $(`#${id} .new_price`).text().trim();
+      const tags = [
+        title.split("[")[0].toLowerCase().trimEnd(),
+        ...title.replace("]", "").split("[")[1].toLowerCase().split(" "),
+      ];
+
+      items.push({
+        title,
+        url: `https://www.pricecharting.com/console/apple-iphone${url}`,
+        tags,
+        usedPrice,
+        newPrice,
+        refurbPrice,
+      });
+    });
+  }
+
+  items.sort((a, b) => b.title.length - a.title.length);
+
+  writeFile("./products.json", JSON.stringify({ items }), (error) => {
+    if (error) {
+      console.log("An error has occurred ", error);
+      return;
+    }
+    console.log("Data written successfully to disk");
+  });
+}
+
+scrape();
